8d7694060d3a1e7376f56601084f75acd846b378
jainish koladiya <155936377+jainish2222@users.noreply.github.com>
2025-05-28 15:59:30 +0530

    Update CompanionCard.tsx

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { removeBookmark } from "@/lib/actions/companion.actions";
 import { addBookmark } from "@/lib/actions/companion.actions";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from "next/navigation";
-
 interface CompanionCardProps {
   id: string;
   name: string;
@@ -14,17 +14,8 @@ interface CompanionCardProps {
   color: string;
   bookmarked: boolean;
 }
-
-const CompanionCard = ({
-  id,
-  name,
-  topic,
-  subject,
-  duration,
-  color,
-  bookmarked,
-}: CompanionCardProps) => {
-  const pathname = usePathname();
+const CompainionCard = ({ id, name, topic, subject, duration, color,bookmarked }:CompanionCardProps) => {
+const pathname = usePathname();
   const handleBookmark = async () => {
     if (bookmarked) {
       await removeBookmark(id, pathname);
@@ -33,24 +24,23 @@ const CompanionCard = ({
     }
   };
   return (
-    <article className="companion-card" style={{ backgroundColor: color }}>
-      <div className="flex justify-between items-center">
-        <div className="subject-badge">{subject}</div>
-        <button className="companion-bookmark" onClick={handleBookmark}>
+    <article className="companion-card relative z-10" style={{ backgroundColor: color }}>
+        <div className='flex justify-between items-center'>
+           <div className="subject-badge">{subject}</div>
+              <button className="companion-bookmark" onClick={handleBookmark}>
           <Image
             src={
-              bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
+               bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
             }
             alt="bookmark"
             width={12.5}
             height={15}
           />
         </button>
-      </div>
-
-      <h2 className="text-2xl font-bold">{name}</h2>
-      <p className="text-sm">{topic}</p>
-      <div className="flex items-center gap-2">
+        </div>
+         <h2 className="text-2xl font-bold">{name}</h2>
+         <p className="text-sm">Topic: {topic}</p>
+        <div className="flex items-center gap-2">
         <Image
           src="/icons/clock.svg"
           alt="duration"
@@ -59,14 +49,13 @@ const CompanionCard = ({
         />
         <p className="text-sm">{duration} minutes</p>
       </div>
-
       <Link href={`/companions/${id}`} className="w-full">
         <button className="btn-primary w-full justify-center">
           Launch Lesson
         </button>
       </Link>
     </article>
-  );
-};
+  )
+}
 
-export default CompanionCard;
+export default CompainionCard
